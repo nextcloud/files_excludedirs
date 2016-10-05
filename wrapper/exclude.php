75f963be94e6025cc4aeedf8c3c1e2bbbfdc5e48
Roeland Jago Douma <roeland@famdouma.nl>
2016-10-05 21:01:23 +0200

    Initial commit of the exclude files app
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ wrapper/exclude.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @copyright 2016 Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\Files_ExcludeDirs\Wrapper;
+
+use OC\Files\Storage\Wrapper\Wrapper;
+
+class Exclude extends Wrapper {
+	/**
+	 * @var string[] Directories to exclude
+	 */
+	private $exclude;
+
+	/**
+	 * @param array $parameters
+	 */
+	public function __construct($parameters) {
+		parent::__construct($parameters);
+		$this->exclude = $parameters['exclude'];
+	}
+
+	/**
+	 * Check if the path contains an ignored direcotry
+	 *
+	 * @param string $path
+	 * @return bool
+	 */
+	private function excludedPath($path) {
+		if ($path === '') {
+			return false;
+		}
+
+		foreach ($this->exclude as $dir) {
+			if (strpos($path, $dir) !== false) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function opendir($path) {
+		if ($this->excludedPath($path)) {
+			return false;
+		}
+
+		return $this->storage->opendir($path);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 * Todo: throw forbiddenexception??
+	 */
+	public function getMetaData($path) {
+		if ($this->excludedPath($path)) {
+			return null;
+		}
+		return $this->getWrapperStorage()->getMetaData($path);
+	}
+}
---
+++ wrapper/manager.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright 2016 Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\Files_ExcludeDirs\Wrapper;
+
+use OC\Files\Filesystem;
+use OC\Files\Storage\Storage;
+
+class Manager {
+
+	public function setupStorageWrapper() {
+		Filesystem::addStorageWrapper('exclude', function ($mountPoint, Storage $storage) {
+			return new Exclude(['storage' => $storage, 'exclude' => ['.snapshot']]);
+		});
+	}
+}
